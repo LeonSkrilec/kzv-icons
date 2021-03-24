9cfbc3fff822853df3334786d46503fcde3dc1c3
Leon Skrilec <l.skrilec@gmail.com>
2021-03-24 18:44:46 +0100

    Updates

---
+++ precache-manifest.25c19f3de0f2dac75f1f3a80212da8a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a0f959ac9d77a2b8d46315448fdf11",
+    "url": "./index.html"
+  },
+  {
+    "revision": "248d3f3d5981afe97570",
+    "url": "./static/css/main.afdeb38d.chunk.css"
+  },
+  {
+    "revision": "fa4bba55c522eaccc573",
+    "url": "./static/js/2.fb198b5c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fb198b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248d3f3d5981afe97570",
+    "url": "./static/js/main.3805eaf9.chunk.js"
+  },
+  {
+    "revision": "fb60d967736f46a60768",
+    "url": "./static/js/runtime-main.1c3f2da7.js"
+  }
+]);
